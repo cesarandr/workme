b161e0bf24c1900c374540be1fa4bd50d5e1f9b9
cesar andres <cesar.l.andres7@gmail.com>
2022-01-20 18:22:11 +0100

    basic structure of save method

--- src/workout.rs
+++ src/workout.rs
@@ -5,6 +5,8 @@ use std::fs::File;
 use std::io::Read;
 use std::collections::HashMap;
 use rand::Rng;
+use std::time::SystemTime;
+use sqlite::{ self, Value };
 
 // Workout definition
 pub struct Workout {
@@ -30,7 +32,7 @@ impl Workout {
     }
 
     pub fn generate_excercises(&mut self) {
-        // limits
+        // get the limit from the settings
         let max_exercises: u32 = self.settings.get(&String::from("maximum exercises")).expect("maximum exercises setting wasn't set").parse().unwrap();
         // stats
         let mut max_intensity = 0;
@@ -39,7 +41,7 @@ impl Workout {
 
         let mut exercise_group = exercise::Exercise::get_group(self.section.clone()).into_iter(); // get all excercises for a specific body section
         let mut rng = rand::thread_rng();
-        let number_of_exercises = rng.gen_range(5..exercise_group.len());
+        let number_of_exercises = rng.gen_range(5..exercise_group.len()); // decide randomly how many exercises the workout will include
 
         // algorithm to fill in exercises
         while max_intensity <= self.intensity && generated_exercises <= number_of_exercises && generated_exercises as u32 <= max_exercises {
@@ -52,7 +54,7 @@ impl Workout {
                     generated_exercises += 1;
                     max_intensity += new_exercise.intensity.clone();
                     full_duration += new_exercise.duration.clone();
-                    self.excercises.insert(new_exercise.exercise.clone(), new_exercise);
+                    self.excercises.insert(new_exercise.exercise.clone(), new_exercise); // add exercises to the exercises hashmap
                 }
             }
         }
@@ -60,9 +62,12 @@ impl Workout {
         let average_exercise_intensity = self.intensity as f32 / generated_exercises as f32;
         let mut max_intensity = 0;
 
+        // generate the reps for every single exercise
+        // we calculate the average intensity for every exercise and 
+        // divide this by the intensity of a single exercise to get the amount of exercises we need to do
         for exercise in self.excercises.values_mut() {
             let reps = (average_exercise_intensity / exercise.intensity as f32).round() as u32;
-            exercise.set_repetitions(reps);
+            exercise.set_repetitions(reps); // also calculates the intensity and duration again
             max_intensity += exercise.intensity;
             full_duration += exercise.duration;
         }
@@ -78,7 +83,7 @@ impl Workout {
 
         self.settings = HashMap::new();
         for line in contents.lines() {
-            if !line.starts_with("#") {                
+            if !line.starts_with("#") { // exclude comments     
                 let line_items: Vec<&str> = line.split(":").collect();
                 let key = line_items[0].trim().to_string();
                 let value = line_items[1].trim().to_string();
@@ -97,5 +102,21 @@ impl Workout {
         for exercise in self.excercises.values() {
             println!("{}: {}x", exercise.exercise, exercise.repetitions());
         }
+    }
+
+    fn generate_breaks() {
+        // make breaks
+    }
+
+    pub fn save(&self) {
+        let connection = sqlite::open("exercises.db").unwrap();
+        let time = SystemTime::now(); // need good conversion for system time into sqlite datatype
+        let mut cursor = connection.prepare("INSERT VALUE INTO workouts (date, intensity, duration, section, estimated_duration) VALUES (:date, :intensity, :duration, :section, :estimated);").unwrap().into_cursor();
+        cursor.bind_by_name(vec![(":date", Value::String(String::from("test"))), 
+                                 (":intensity", Value::Integer(self.intensity as i64)), 
+                                 (":duration", Value::Integer(self.duration as i64)),
+                                 (":section", Value::String(self.section.to_string())),
+                                 (":estimated", Value::Integer(self.duration as i64)) ]).unwrap();
+        cursor.next().unwrap();
     }
 }
