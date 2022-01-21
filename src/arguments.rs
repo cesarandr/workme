6e27e68b5bdbd8b8b2fda86c9cf9b68a16e0fc9f
cesar andres <cesar.l.andres7@gmail.com>
2022-01-21 14:50:43 +0100

    updated arguments structure

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,12 +1,33 @@
-use clap::Parser;
+use clap::{self, Parser, Args, Subcommand, ArgEnum};
 
 /// A program wich generates a workout customized for your fitness level
-#[derive(Parser, Debug)]
+/// It tracks your progress and lets you monitor your progression
+#[derive(Parser)]
 #[clap(author, version, about, long_about = None, name = "workme")]
-pub struct Args {
-    #[clap(help = "choose wich body section to train", possible_values = ["upper", "core", "lower", "full"])]
-    pub section: String,
+pub struct Arguments {
 
-    #[clap(help = "choose how hard your workout will be")]
-    pub intensity: u32
+    #[clap(subcommand)]
+    pub command: Option<Commands>,
+
 }
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// change your account
+    Account {
+        #[clap(help = "choose your account")]
+        name: String
+    },
+    /// start your workout
+    Start {
+        #[clap(help = "choose a body section to train", possible_values = ["upper", "core", "lower", "full"])]
+        section: String,
+        #[clap(help = "choose the intensity of the workout")]
+        intensity: u32
+    },
+    /// stop a started workout
+    Stop
+
+}
+
+
