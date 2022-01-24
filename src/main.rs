9493553ce5cffaae51b359b26edbc8a19d677b5e
cesar andres <cesar.l.andres7@gmail.com>
2022-01-24 17:56:30 +0100

    view changes to test the others

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,34 @@ mod workout;
 mod arguments;
 
 
-use arguments::Args;
+use arguments::Arguments;
+use arguments::Commands;
 use clap::Parser;
 use std::str::FromStr;
 
 
 fn main() {
-    let args = Args::parse();
-    let mut workout = workout::Workout::new(args.intensity, workout::body_section::BodySection::from_str(&args.section).unwrap());
-    workout.new_settings("src/workout/settings.txt");
-    workout.generate_excercises();
-    workout.display_workout();
+    let args = Arguments::parse();
+    let mut workout: workout::Workout;
+    let mut started_workout = false;
+
+    match args.command {
+        Some(Commands::Start { section, intensity }) => {
+            workout = workout::Workout::new(intensity, workout::body_section::BodySection::from_str(&section).unwrap());
+            workout.new_settings("src/workout/settings.txt");
+            //workout.generate_excercises();
+            workout.display_workout();
+            workout.save();
+        },
+        Some(Commands::Stop) => {
+
+        },
+        Some(Commands::Account { name }) => println!("Account changed"),
+        None => println!("Error")
+    }
+    
+
+
 }
 
 
